53c06582c505873d3090b2f53908d2c7f7038af0
kwhong95 <kwhong95@kakao.com>
2021-06-21 22:17:56 +0900

    [Day1]Create Section1 Template

---
+++ pages/section1/elements.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const Wrap = styled.div``;
+
+export const Section = styled.section`
+  background: #ddd;
+`;
+
+export const Title = styled.h2`
+  padding:100px 0 80px; 
+  font-size:70px; 
+  text-align: center;  
+`;
+
+export const Text = styled.p`
+  max-width:70%; 
+  margin:0 auto; 
+  font-size:30px; 
+  text-align: center;
+`;
+
+export const Content = styled.article`
+  font-size:170px;
+`;
+
+export const Progress = styled.div`
+  position:fixed; 
+  left:50%;
+  top:500px; 
+  width:70%; 
+  height:100px; 
+  margin-left:-35%; 
+  border:1px solid #222;; 
+  text-align: center;
+`;
+
+export const Bar = styled.span`
+  display: block; 
+  width:0; 
+  height:100%;
+  background-color: #111;
+`;
--- pages/section1/index.js
+++ pages/section1/index.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from 'react'
+import {
+    Bar,
+    Content,
+    Progress,
+    Section,
+    Text,
+    Title,
+    Wrap
+} from "./elements"
+import $ from 'jquery';
 
 const Section1 = () => {
+
     return (
-        <div>
-            Section 1 Page
-        </div>
+        <Wrap>
+          <Section>
+            <Title>스크롤 백분율 구하기</Title>
+            <Text>전체 페이지의 높이와 스크롤 높이를 계산하여 게이지화 한다.</Text>
+            <Content>
+              <Progress>
+                <Bar />
+                <Text>0%</Text>
+              </Progress>
+                가<br/>
+                나<br/>
+                다<br/>
+                라<br/>
+                마<br/>
+                바<br/>
+                사<br/>
+                아<br/>
+                자<br/>
+                차<br/>
+                카<br/>
+                타<br/>
+                파<br/>
+                하
+            </Content>
+          </Section>
+        </Wrap>
     )
 }
 
-export default Section1;
+export default Section1
+

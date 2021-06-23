2dcd8c64af9ff7ccbc6fbdaaea7527c16c45fec3
kwhong95 <kwhong95@kakao.com>
2021-06-23 19:44:29 +0900

    [Day1]Section 2 - Create Useable Function & Add Scroll Event Listenner

--- pages/section2/index.js
+++ pages/section2/index.js
@@ -1,41 +1,75 @@
-import React from 'react';
-import {Figure, Item, List, SC_Infinity, Section, Title, Wrap} from "./elements";
+import React, { useEffect } from 'react';
+import {
+    Figure,
+    Item,
+    List,
+    SC_Infinity,
+    Section,
+    Title,
+    Wrap
+} from "./elements";
+import Image from 'next/image';
+
+import Img1 from './images/1.jpg';
+import Img2 from './images/2.jpg';
+import Img3 from './images/3.jpg';
+import Img4 from './images/4.jpg';
 
 const Section2 = () => {
-  return (
+
+    const getList = () => {};
+
+    const listCall = () => {};
+
+    useEffect(() => {
+        window.addEventListener('scroll', listCall);
+        listCall();
+    })
+
+    return (
       <Wrap>
-          <Section>
+          <Section onScroll={listCall}>
               <Title>INFINITE SCROLL</Title>
               <SC_Infinity>
                   <List>
                       <Item>
                           <Figure>
-                              <img src='./images/1.jpg' alt='img'/>
+                              <Image src={Img1} alt='img'/>
                           </Figure>
                       </Item>
                       <Item>
                           <Figure>
-                              <img src='./images/2.jpg' alt='img' />
+                              <Image src={Img2} alt='img' />
                           </Figure>
                       </Item>
                       <Item>
                           <Figure>
-                              <img src='./images/3.jpg' alt='img' />
+                              <Image src={Img3} alt='img' />
                           </Figure>
                       </Item>
                       <Item>
                           <Figure>
-                              <img src='./images/1.jpg' alt='img' />
+                              <Image src={Img4} alt='img' />
                           </Figure>
                       </Item>
                       <Item>
                           <Figure>
-                              <img src='./images/2.jpg' alt='img' />
+                              <Image src={Img1} alt='img' />
                           </Figure>
                       </Item>
                       <Item>
                           <Figure>
-                              <img src='./images/3.jpg' alt='img' />
+                              <Image src={Img2} alt='img' />
+                          </Figure>
+                      </Item>
+                      <Item>
+                          <Figure>
+                              <Image src={Img3} alt='img' />
+                          </Figure>
+                      </Item>
+                      <Item>
+                          <Figure>
+                              <Image src={Img4} alt='img' />
                           </Figure>
                       </Item>
                   </List>

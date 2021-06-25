db4b44f04760a173cf66e6694c36422eee68ac42
kwhong95 <kwhong95@kakao.com>
2021-06-25 18:44:56 +0900

    [SEC4]Create Keyframe & Play animation But Dont Play Bounce Animation...

--- pages/section4/elements.js
+++ pages/section4/elements.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrap = styled.div`
   overflow: hidden;
@@ -12,5 +12,61 @@ export const Title = styled.h2`
 `;
 
 export const Figure = styled.figure`
+  animation-name: move_model;
   
+  // 한번 재생 시 걸리는 시간
+  animation-duration: 6s;
+  // 지연 시간 (몇초 후 시작?)
+  animation-delay: 0s;
+  // 애니메이션 재생 방향 설정 alternate: 순방향, reverse: 역방향
+  animation-direction: alternate;
+  // 애니메이션 재생 횟수 infinite: 무한 재생
+  animation-iteration-count: infinite;
+  // 애니메이션 재생 여부 running: 재생(기본값), paused: 정지
+  animation-play-state: paused;
+  // 애니메이션 가속도 설정 linear, ease, ease-in, ease-out, custom etc..
+  animation-timing-function: linear;
+  /* forwards:애니메이션이 끝날 때 요소의 마지막 키 프레임의 스타일 값을 유지, 
+     backwards:첫 번째 키 프레임에 의해 설정된 스타일 값을 얻음, 
+     both:애니메이션이 시작되기 전에 첫 번째 키 프레임에서 설정 한 스타일 값을 가져 오도록하고 애니메이션이 끝날 때 마지막 키 프레임의 스타일 값을 유지 */
+  animation-fill-mode: forwards;
+  
+  @keyframes move_model {
+    0% {
+      transform: translate(0px, 0px);
+    }
+    50% {
+      transform: translate(200px, 0);
+    }
+    75% {
+      .bounce {
+        position: relative;
+        animation: bounce 2s infinite linear;
+        @keyframes bounce {
+        0% {
+          top: 0;
+        }
+        50% {
+          top: -5px;
+        }
+        70% {
+          top: -50px;
+        }
+        100% {
+          top: 0;
+        }
+    100% {
+      display: none;
+    }
+  }  
+      }
+    }
+  }
+  
+  
+  ${props =>
+    props.active &&
+    css `
+      animation-play-state: running;
+    `}
 `;
--- pages/section4/index.js
+++ pages/section4/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     Title,
     Wrap,
@@ -8,10 +8,16 @@ import Image from "next/image";
 import Car from './Car.png'
 
 const Section4 = () => {
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => { setActive(true) }, 2000);
+    })
+
     return (
         <Wrap>
             <Title>CSS ANIMATION KEYFRAME</Title>
-            <Figure>
+            <Figure active={active}>
                 <Image src={Car} width={300} height={300}/>
             </Figure>
         </Wrap>
